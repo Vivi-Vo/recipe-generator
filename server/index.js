1ccf80cb18a5d4abf60917a944a9a9adccc1952c
Vivi-Vo <vivivo5015@gmail.com>
2020-10-19 00:38:32 -0400

    Adding dist folder for deploying Vuejs on Heroku

--- server/index.js
+++ server/index.js
@@ -1,11 +1,15 @@
 const express = require('express');
 const cors = require('cors');
+const path = require('path');
+const serveStatic = require('serve-static');
 const app = express();
-app.use(cors());
 const recipes = require('./routes/api/recipes');
-app.use('/recipe', recipes);
 
-const port = process.env.PORT || 4000;
+app.use(cors());
+
+app.use('/', serveStatic(path.join(__dirname, '/client/dist')))
+app.use('/recipe', recipes);
+const port = process.env.PORT || 5000;
 
 app.listen(port, () => console.log(`Server started on port ${port}`));
 
