5d699457b6b678900d1474717976bbf66b5a24b3
Vivi-Vo <vivivo5015@gmail.com>
2021-04-01 23:49:49 -0400

    Recipe card
    - Refactor recipe card as a separate component
    - Update UI for recipe card

--- client/src/components/RecipeCard.vue
+++ client/src/components/RecipeCard.vue
@@ -1,46 +1,38 @@
 <template>
-      <v-dialog id="popup" v-model="dialog">
-      <v-card id="card">
-        <v-card-title id="dishTitle">
-          <span class="headline">{{selectedDish.title}}</span>
-        </v-card-title>
+  <v-card class="pa-6">
+    <h1>{{title}}</h1>
+    <img :src="recipeURL" />
+    <h2>{{ timeCook }} minutes | {{ingredients.length}} ingredients</h2>
+    <h2>Ingredients:</h2>
 
-        <v-avatar size="200" >
-          <v-img :src="recipeURL" />
-        </v-avatar>        
+    <ul>
+      <li v-for="item in ingredients" :key="item.index">{{ item }}</li>
+    </ul>
 
-      <v-card-text>
-          <span class="headline">Time Cook: {{timeCook}} minutes</span>
-        </v-card-text>
-
-        <v-btn class="btn mb-4" raised color="purple lighten-4" @click="showIngredient=!showIngredient">Ingredients</v-btn>
-        <v-card-text>
-          <ul>
-            <li v-for="item in ingredients" :key="item.index">{{item}}</li>
-          </ul>
-        </v-card-text>
-
-        <v-spacer></v-spacer>
-        <v-btn raised color="purple lighten-4" @click="showInstruction=!showInstruction">Direction</v-btn> 
-
-
-        <v-card-text >
-          <ol>
-            <li v-for="steps in instructions" :key="steps.number">{{steps.step}}</li>
-          </ol>
-        </v-card-text>
-
-        <v-card-text v-if="instructionURL">
-          <a :href="instructionURL" target="_blank">Recipe URL</a>
-          <p></p>
-        </v-card-text>
-
-        <v-card-actions>
-          <v-btn icon @click="scrollToTop">
-            <v-icon>mdi-arrow-up-circle</v-icon>
-          </v-btn>
-          <v-btn absolute bottom right color="green darken-1" text @click="dialog =false">Back</v-btn>
-        </v-card-actions>
-      </v-card>
-    </v-dialog>
+    <h2>Step-by-step instruction:</h2>
+    <ol>
+      <li v-for="steps in instructions" :key="steps.number">
+        {{ steps.step }}
+      </li>
+    </ol>
+  </v-card>
 </template>
+
+<script>
+export default {
+  props: {
+    timeCook: String,
+    ingredients: Array,
+    instructionURL: String,
+    recipeURL: String,
+    instructions: Array,
+    title: String
+  },
+};
+</script>
+
+
+<style scoped>
+@import "../../assets/styles/recipes.scss";
+@import url("https://use.typekit.net/bdn8fqt.css");
+</style>
