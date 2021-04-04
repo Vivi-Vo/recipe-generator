28c0d294e7c576ddff46569e99b78393854c9309
Vivi-Vo <vivivo5015@gmail.com>
2021-04-04 19:41:05 -0400

    minor UI changes

--- server/routes/api/recipes.js
+++ server/routes/api/recipes.js
@@ -6,26 +6,24 @@ const url_base = 'https://api.spoonacular.com/recipes';
 
 router.get('/:ingredients', (req, res) => {
   const params = new URLSearchParams({
-  //  apiKey: process.env.VUE_APP_API_KEY,
-  apiKey: "b51e4d59d50246cc93a8a0545843b45c",  
-  ingredients: req.params.ingredients
+    apiKey: process.env.VUE_APP_API_KEY,
+    ingredients: req.params.ingredients
   });
   const url = `${url_base}/findByIngredients?${params}`;
-  console.log(url);
   fetch(url)
     .then((res) => res.json())
-      .then((data) => res.json(data))
+    .then((data) => res.json(data))
 });
 
 router.get('/instructions/:id', (req, res) => {
   const id = req.params.id
   const params = new URLSearchParams({
-  //  apiKey: process.env.VUE_APP_API_KEY,
-  apiKey: "b51e4d59d50246cc93a8a0545843b45c",  
-  }); 
+    apiKey: process.env.VUE_APP_API_KEY,
+    ranking: 2
+  });
 
   const url = `${url_base}/${id}/information?${params}`;
   fetch(url).then((res) => res.json()).then(data => res.json(data))
 });
 
-module.exports = router; 
+module.exports = router;
